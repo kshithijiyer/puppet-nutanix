5cd1ca41cb30d7af599679218d58f9c6553fbf9c
Jon Kohler <jon@nutanix.com>
2018-06-13 23:54:07 -0400

    class

---
+++ manifests/nutanix.pp
@@ -0,0 +1,9 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include nutanix_enterprise_cloud::nutanix
+class nutanix_enterprise_cloud::nutanix {
+    include nutanix_enterprise_cloud::nutanix
+}
